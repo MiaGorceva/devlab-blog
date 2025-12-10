49ca631c17d81edea7fe122af321984a5e22055e
MiaForEva <78581869+MiaGorceva@users.noreply.github.com>
2025-12-10 23:53:43 +0300

    test nav

--- js/script.js
+++ js/script.js
@@ -146,11 +146,15 @@
 // === Мобильное меню ===
 var navToggle = document.querySelector(".nav-toggle");
 var navGroup  = document.querySelector(".nav-group");
-var navCta    = document.querySelector(".nav-cta");
 
 if (navToggle && navGroup) {
   navToggle.addEventListener("click", function () {
+
     var isOpen = document.body.classList.toggle("nav-open");
+
+    // переключаем иконку
+    navToggle.textContent = isOpen ? "×" : "☰";
+
     navToggle.setAttribute("aria-expanded", isOpen ? "true" : "false");
   });
 }
