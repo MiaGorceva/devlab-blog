af53f3954bcf278e945236b68e6f11dbcee85c62
MiaForEva <78581869+MiaGorceva@users.noreply.github.com>
2025-12-10 13:27:10 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -126,7 +126,7 @@
     //var scriptId =
    //   "https://script.google.com/macros/s/AKfycbw63ZiEXuv9Hw-0tHmUaa2fVvpee2Eluwc9O9_J7BKAQwICReHKcULyP3lb2dbHxbxYZg/exec";
     var url =
-      "https://script.google.com/macros/s/AKfycbxgVm8l1dx2IO7e7O5S0rXQmZugHWr2xeCb4eulOnyiIRRBFcIgQ3kC3K2pHgg0K6NeNg/exec"
+      "https://script.google.com/macros/s/AKfycbx_U2_PCC4qZprp3HsrwSOEl2Kr_p-YMjjeD2JALTZqT9VhnMCrpuFAlO_gkkjOmH2Dtg/exec"
     + "?post=" + encodeURIComponent(postId);
     //  scriptId +
     //  "/exec?post=" +
